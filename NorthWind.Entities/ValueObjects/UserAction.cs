bba68fe13d7e8cec1590215ab3713ee99c7441e6
Carlos Loitti <carlosloitir@gmail.com>
2024-10-09 15:19:25 -0500

    Clase UserAction

---
+++ NorthWind.Entities/ValueObjects/UserAction.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthWind.Entities.ValueObjects
+{
+    internal class UserAction (string user, string description)
+    {
+
+        public DateTime CreatedDateTime { get;} = DateTime.Now;
+
+        public string User => user;
+
+        public string Description => description;
+    }
+}
