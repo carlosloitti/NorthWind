ae622ad0b475036f4764efcf58df53b635728953
Carlos Loitti <carlosloitir@gmail.com>
2024-10-09 16:24:47 -0500

    Ajustando Salida por consola + Filewriter

--- NorthWind.ConsoleApp/Program.cs
+++ NorthWind.ConsoleApp/Program.cs
@@ -2,8 +2,12 @@
 
 using NorthWind.ConsoleApp.Services;
 using NorthWind.Entities.Interfaces;
+using NorthWind.Writers;
 
-IUserActionWriter Writer = default;
+IUserActionWriter Writer = new ConsoleWriter();
 
 AppLogger Logger = new AppLogger(Writer);
 Logger.WriteLog("Aplicacion started.");
+
+ProductService Service = new ProductService(Writer);
+Service.Add("Demo", "Azucar refinada");
